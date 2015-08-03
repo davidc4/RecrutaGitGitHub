e6335284913c9558492e9d0f69036e05c10db87b
David Santos <david@compufour.com.br>
2015-08-03 17:39:16 -0300

    novo método

--- index.php
+++ index.php
@@ -8,4 +8,8 @@ $app->get('/hello/{name}', function($name) use($app) {
     return 'Hello '.$app->escape($name);
 });
 
+$app->get('/', function () user($app) {
+    return 'OK';
+});
+
 $app->run();
